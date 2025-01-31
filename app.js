a614acf566cf65b42f09cfc8eafe8ec70a137f5a
Lil Vanilla <kalebrwalter@gmail.com>
2025-01-31 06:11:46 -0800

    bugfix/ rearrange imports

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const mainRouter = require("./routes/index");
 const helmet = require("helmet");
+const mainRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
